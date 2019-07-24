cbed9df5360defd7460600aa9d42dcd1f4d319a7
ginarizkaa <ginarizkaa@gmail.com>
2019-07-24 16:59:46 +0700

    second commit

--- common/models/employee.js
+++ common/models/employee.js
@@ -3,6 +3,87 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-module.exports = function(Employee) {
+module.exports = function(Employee) { //nama class
+    Employee.remoteMethod(
+        'getByName', 
+        {
+            description: 'get name like -> gina',
+            accepts:[
+                {arg: 'firstname', type: 'string'}
+            ],
+            returns: {
+                arg: 'res', type: 'object', root: true
+            },
+            http: { path: '/getByName', verb: 'get'}
+        }
+    );
+    Employee.getByName = function(firstname, callback){
+        new Promise(function(resolve, reject){ //fungsi promise untuk menjalankan code sesuai dengan urutannya
+            
+            var filter = {
+                where: {
+                    first_name : { //nama field
+                        like : firstname
+                    }
+                }
+            }
+            Employee.find(filter, function(err, result){
+                if(err) reject (err)
+                if (result === null){
+                    err = new Error('Cannot find that name')
+                    err.statusCode = 404
+                    reject (err)
+                }
 
+                resolve (result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
+
+    Employee.remoteMethod(
+        'getByLastName', 
+        {
+            description: 'get name like -> gina',
+            accepts:[
+                {arg: 'lastname', type: 'string'}
+            ],
+            returns: {
+                arg: 'res', type: 'object', root: true
+            },
+            http: { path: '/getByLastName', verb: 'get'}
+        }
+    );
+    Employee.getByLastName = function(lastname, callback){
+        new Promise(function(resolve, reject){ //fungsi promise untuk menjalankan code sesuai dengan urutannya
+            
+            var filter = {
+                where: {
+                    last_name : { //nama field
+                        like : lastname
+                    }
+                }
+            }
+            Employee.find(filter, function(err, result){
+                if(err) reject (err)
+                if (result === null){
+                    err = new Error('Cannot find that name')
+                    err.statusCode = 404
+                    reject (err)
+                }
+
+                resolve (result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
+    
 };
--- common/models/report.js
+++ common/models/report.js
@@ -3,6 +3,106 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+var app = require('../../server/server')
 module.exports = function(Report) {
+    Report.remoteMethod(
+        'getByDepartemen',
+        {
+            description: 'get departemen like -> Penjualan',
+            accepts: [
+                {arg: 'departemen', type: 'string'}
+            ],
+            returns: {
+                arg: 'res', type: 'object', root: true
+            },
+            http: { path: '/getByDepartemen', verb: 'get'}
+        }
+    );
+    Report.getByDepartemen = function(departemen, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    departemen : {
+                        like : departemen
+                    }
+                }
+            }
+    
+            Report.find(filter, function(err, result){
+                if (err) reject (err)
+                if (result === null){
+                    err = new Error('Cannot find that department name')
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve (result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback(null, res)
+        }).catch(function(err){
+            callback(err);
+        })
+    }
 
+    Report.remoteMethod(
+        'getDetailById',
+        {
+            description: 'get detail by id',
+            accepts: [
+                {arg: 'id', type: 'string'}
+            ],
+            returns: {
+                arg: 'res', type: 'object', root: true
+            },
+            http: { path: '/getDetailById', verb: 'get'}
+        }
+    );
+    Report.getDetailById = function(id, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    id : id
+                }
+            }
+    
+            Report.find(filter, function(err, result){
+                if (err) reject (err)
+                if (result === null){
+                    err = new Error('Cannot find that id')
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve (result)
+                
+            })
+        }).then(function(res){
+            //console.log(res)
+            var client = app.models.Client
+
+            var clientId = res[0].clientId
+
+            var filter = {
+                where : {
+                    id : clientId
+                }
+            }
+
+            client.find(filter, function(err, resclient){
+                if (err) reject (err)
+                if (resclient === null){
+                    err = new Error('Cannot find that id')
+                    err.statusCode = 404
+                    reject (err)
+                }
+                
+                res[0].nama_client = resclient[0].nama_client
+                res[0].asal_perusahaan = resclient[0].asal_perusahaan
+
+                return callback(null, res)
+            })
+        }).catch(function(err){
+            callback(err);
+        })
+    }
 };
